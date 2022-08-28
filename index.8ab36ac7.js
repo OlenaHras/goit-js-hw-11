e42712c7b0c32aa278d96fed0af712d9133a4d7c
OlenaHras <100197147+OlenaHras@users.noreply.github.com>
2022-08-28 13:17:20 +0000

    Deploying to gh-pages from @ OlenaHras/goit-js-hw-11@dd8171258bf1a2fc6d2ac56950b08209b01b216d 🚀

---
+++ index.8ab36ac7.js
@@ -0,0 +1,2 @@
+document.querySelector("#gotoTop").addEventListener("click",(function(){$("html, body").animate({scrollTop:0},"slow")}));window.onscroll=function(){document.body.scrollTop>20||document.documentElement.scrollTop>20?document.getElementById("gotoTop").style.display="block":document.getElementById("gotoTop").style.display="none"};
+//# sourceMappingURL=index.8ab36ac7.js.map
